f8ee862f4be09374fc108bdf3b1af42d678dc395
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-05-15 16:54:43 -0400

    A file opening function. It can be used for any kind of file, I used it for fsx and it's various addons, to save me time doing it this way.

---
+++ fsx_open.py
@@ -0,0 +1,11 @@
+import os
+
+def open_file(test):
+    os.startfile(test)
+
+
+# This is for opening all of my fsx files at once with one click. 
+open_file(r'E:\Steam\steamapps\common\FSX\fsx.exe')
+open_file(r'C:\Aerosoft\FSC9\FSC.exe')
+open_file(r'C:\Program Files (x86)\HiFi\AS16_FSX\AS16.exe')
+open_file(r'C:\Program Files (x86)\rcv4x\rcv4.exe')
