16805bb7b18c11c7eb90ccaee3a34282fa344446
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-05-17 21:29:45 -0400

    Changed the open location to the VR add on.

--- fsx_open.py
+++ fsx_open.py
@@ -5,7 +5,7 @@ def open_file(test):
 
 
 # This is for opening all of my fsx files at once with one click. 
-open_file(r'E:\Steam\steamapps\common\FSX\fsx.exe')
+open_file(r'E:\Steam\steamapps\common\FSX\FlyInsideFSX.exe')
 open_file(r'C:\Aerosoft\FSC9\FSC.exe')
 open_file(r'C:\Program Files (x86)\HiFi\AS16_FSX\AS16.exe')
 open_file(r'C:\Program Files (x86)\rcv4x\rcv4.exe')
