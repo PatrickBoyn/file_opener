cb3fb3f3624681ca2e6b3aa3b61f659db8b57c90
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-05-21 01:16:40 -0400

    More refactoring. Included the args argument so the file paths could be passed as arguments.  Also added a for loop to append the file paths to the list.

--- file_opener.py
+++ file_opener.py
@@ -1,14 +1,13 @@
 import os
 
 
-def file_opener():
+def file_opener(*args):
     # List all of the files you want to open here. 
-    files = [
-        r'E:\Steam\steamapps\common\FSX\fsx.exe',
-        r'C:\Aerosoft\FSC9\FSC.exe',
-        r'C:\Program Files (x86)\HiFi\AS16_FSX\AS16.exe',
-        r'C:\Program Files (x86)\rcv4x\rcv4.exe'
-    ]
+    files = []
+
+    # For adding the arguments to the list.
+    for a in args:
+        files.append(a)
 
     # This function runs the files
     # The best way of doing this is passing a list.
@@ -16,10 +15,13 @@ def file_opener():
         os.startfile(ls)
     
 
-    # This is for opening all of my fsx files at once with one click. 
+    # This is for opening all of my fsx files at once with one click.
     for a in files:
         open_file(a)
         print(a)
 
 
-file_opener()
+# Put the files paths in as parameters.
+file_opener(r'E:\Steam\steamapps\common\FSX\fsx.exe',
+            r'C:\Aerosoft\FSC9\FSC.exe', r'C:\Program Files (x86)\HiFi\AS16_FSX\AS16.exe',
+            r'C:\Program Files (x86)\rcv4x\rcv4.exe')
