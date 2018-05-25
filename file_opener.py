410f617752af99e40546e3c4d580efe79e243f23
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-05-25 02:19:21 -0400

    Tried out something different, but didn't work, back to the working code.

--- file_opener.py
+++ file_opener.py
@@ -8,14 +8,12 @@ def file_opener(*args):
     # For adding the arguments to the list.
     for a in args:
         files.append(a)
-
     # This is for opening all of my fsx files at once with one click.
     for b in files:
         os.startfile(b)
         # The print statement shows me if everything was read ok, if something
         # isn't opening correctly for example. 
         print(b)
-
 
 # Put the files paths in as parameters.
 file_opener(r'E:\Steam\steamapps\common\FSX\fsx.exe',
