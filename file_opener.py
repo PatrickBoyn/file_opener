1f9fba384bf6830e4b57a19e5f33693e5b8f1521
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-06-06 15:05:31 -0400

    Moved the file_opener function into the Window class, and made most of the necessary adjustments to turn this into a GUI, rather than a command line file_opener.

--- file_opener.py
+++ file_opener.py
@@ -10,30 +10,36 @@ class Window(QtWidgets.QWidget):
 
     def init_ui(self):
         self.setWindowTitle('File Opener')
+        self.l = QtWidgets.QLabel()
+        self.le = QtWidgets.QLineEdit()
+        self.b = QtWidgets.QPushButton('Open Files')
+        self.l.setText('Test')
+
+        self.b.clicked.connect(self, self.file_opener)
+
+        v_box = QtWidgets.QVBoxLayout()
+        v_box.addWidget(self.le)
+        v_box.addWidget(self.b)
+
+        self.setLayout(v_box)
+
         self.show()
 
+    def file_opener(self, *args):
+        # List all of the files you want to open here.
+        self.files = []
 
-def file_opener(*args):
-    # List all of the files you want to open here. 
-    files = []
-
-    # For adding the arguments to the list.
-    for a in args:
-        files.append(a)
-    # This is for opening all of my fsx files at once with one click.
-    for b in files:
-        os.startfile(b)
-        # The print statement shows me if everything was read ok, if something
-        # isn't opening correctly for example. 
-        print(b)
+        # For adding the arguments to the list.
+        for a in args:
+            self.files.append(a)
+            # This is for opening all of my fsx files at once with one click.
+            for b in self.files:
+                os.startfile(b)
+                # The print statement shows me if everything was read ok, if something
+                # isn't opening correctly for example.
+                print(b)
 
 
 app = QtWidgets.QApplication(sys.argv)
 a_window = Window()
 sys.exit(app.exec_())
-
-# Put the files paths in as parameters.
-# file_opener(r'E:\Steam\steamapps\common\FSX\fsx.exe',
-#             r'C:\Aerosoft\FSC9\FSC.exe', 
-#             r'C:\Program Files (x86)\HiFi\AS16_FSX\AS16.exe',
-#             r'C:\Program Files (x86)\rcv4x\rcv4.exe')
