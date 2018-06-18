c8ccb55a82db5ce8979d15a33a3218d2718bcd6b
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-06-18 16:31:38 -0400

    Trying to work on a module system.

--- file_opener.py
+++ file_opener.py
@@ -1,21 +1,5 @@
-import tkinter
 import os
-
-root = tkinter.Tk()
-
-# TODO find a way of making this simpler.
-text_var = tkinter.StringVar()
-text_var2 = tkinter.StringVar()
-text_var3 = tkinter.StringVar()
-
-text_variable = [text_var, text_var2, text_var3]
-
-# This is hard coded because these are the main ones
-# I will use with this program. I wanted a GUI for easy change if I wanted to add,
-# or change these files in the future.
-path1 = r'E:\Steam\steamapps\common\FSX\fsx.exe'
-path2 = r'C:\Program Files (x86)\HiFi\AS16_FSX\AS16.exe'
-path3 = r'C:\Program Files (x86)\rcv4x\rcv4.exe'
+from window_conf import *
 
 
 def main():
@@ -31,18 +15,6 @@ def main():
 	btn.grid(row=0, column=1)
 
 	root.mainloop()
-
-
-def window_conf(title, geometry):
-	root.title(title)
-	root.geometry(geometry)
-
-
-def text_box(variable, index, path, row, column):
-	box = tkinter.Entry(textvariable=variable)
-	box.insert(index, path)
-	box.grid(row=row, column=column, padx=5)
-	print(path)
 
 
 def start_file():
