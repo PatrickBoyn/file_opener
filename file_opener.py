b9099d7d11ff7e860f50d8a96bc566a2b90e2cdd
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-06-18 21:15:46 -0400

    Spaced instead of 'tabbed.' Apparently suddenly it decided I was using mixes of tabs and spaces.

--- file_opener.py
+++ file_opener.py
@@ -5,19 +5,19 @@ import startup
 
 
 def main():
-	# Window title, dimensions.
-	window_conf.window_conf('File Opener', '180x70')
+    # Window title, dimensions.
+    window_conf.window_conf('File Opener', '180x70')
 
-	# Textvariable, index, path, row, column
-	window_conf.text_box(text_var, 0, path1, 0, 0)
-	window_conf.text_box(text_var2, 1, path2, 1, 0)
-	window_conf.text_box(text_var3, 2, path3, 2, 0)
+    # Textvariable, index, path, row, column
+    window_conf.text_box(text_var, 0, path1, 0, 0)
+    window_conf.text_box(text_var2, 1, path2, 1, 0)
+    window_conf.text_box(text_var3, 2, path3, 2, 0)
 
-	window_conf.button('Open', 0, 1, startup.start_file)
-	window_conf.button('Add', 1, 1)
+    window_conf.button('Open', 0, 1, startup.start_file)
+    window_conf.button('Add', 1, 1)
 
-	root.mainloop()
+    root.mainloop()
 
 
 if __name__ == '__main__':
-	main()
+    main()
