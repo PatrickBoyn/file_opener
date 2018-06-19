7d00f385cb3c9819d3a668ac035e660255c1e4b7
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-06-18 20:56:34 -0400

    Trying my best not to import *.

--- file_opener.py
+++ file_opener.py
@@ -1,21 +1,21 @@
-# It's bad practice to import *, but I think it's ok
-# in this case since it's such a small file.
-from window_conf import *
+import window_conf
+from window_conf import (text_var, text_var2, text_var3,
+                         path1, path2, path3)
 import startup
 
 
 def main():
 	# Window title, dimensions.
-	window_conf('File Opener', '180x70')
+	window_conf.window_conf('File Opener', '180x70')
 
 	# Textvariable, index, path, row, column
-	text_box(text_var, 0, path1, 0, 0)
-	text_box(text_var2, 1, path2, 1, 0)
-	text_box(text_var3, 2, path3, 2, 0)
+	window_conf.text_box(text_var, 0, path1, 0, 0)
+	window_conf.text_box(text_var2, 1, path2, 1, 0)
+	window_conf.text_box(text_var3, 2, path3, 2, 0)
 
-	button('Open', 0, 1, startup.start_file)
+	window_conf.button('Open', 0, 1, startup.start_file)
 
-	root.mainloop()
+	window_conf.root.mainloop()
 
 
 if __name__ == '__main__':
