98ade0dee0489e778eba37b171b600d1e49ef586
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-06-19 15:38:53 -0400

    Imported the lists, rather than the text_variable and paths, this way I do not have to import each individual path or variable. I also imported window_conf, and text_box to shorten the length of code needed for both.

--- file_opener.py
+++ file_opener.py
@@ -1,17 +1,18 @@
 import window_conf
-from window_conf import (text_var, text_var2, text_var3,
-                         path1, path2, path3, root)
+from window_conf import (text_variable, text_box,
+                         paths, window_conf,
+                         root)
 import startup
 
 
 def main():
     # Window title, dimensions.
-    window_conf.window_conf('File Opener', '180x70')
+    window_conf('File Opener', '180x70')
 
     # Textvariable, index, path, row, column
-    window_conf.text_box(text_var, 0, path1, 0, 0)
-    window_conf.text_box(text_var2, 1, path2, 1, 0)
-    window_conf.text_box(text_var3, 2, path3, 2, 0)
+    text_box(text_variable[0], 0, paths[0], 0, 0)
+    text_box(text_variable[1], 1, paths[1], 1, 0)
+    text_box(text_variable[2], 2, paths[2], 2, 0)
 
     window_conf.button('Open', 0, 1, startup.start_file)
     window_conf.button('Add', 1, 1)
